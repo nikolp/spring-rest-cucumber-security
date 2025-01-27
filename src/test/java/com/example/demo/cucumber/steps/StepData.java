047f8bfa4bd919202a64c51fd1b9a45eb2e7f746
Philip Nikolov <philip.k.nikolov@gmail.com>
2025-01-27 17:45:37 -0500

    lint

--- src/test/java/com/example/demo/cucumber/steps/StepData.java
+++ src/test/java/com/example/demo/cucumber/steps/StepData.java
@@ -4,8 +4,6 @@ import com.example.demo.Person;
 import com.example.demo.PersonListResponse;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
 public class StepData {
     ResponseEntity<PersonListResponse> personListResponse;
     ResponseEntity<Person> personResponse;

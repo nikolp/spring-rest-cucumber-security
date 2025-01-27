abc2d88e9da7a2b93178953bf242ac027adafefe
Philip Nikolov <philip.k.nikolov@gmail.com>
2025-01-27 17:43:59 -0500

    lombok works

--- src/main/java/com/example/demo/Person.java
+++ src/main/java/com/example/demo/Person.java
@@ -5,39 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     String firstName;
     String lastName;
-
-    public Person() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
